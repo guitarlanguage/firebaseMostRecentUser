e2408d78fc433258618115ba37fa4443b7c7c19a
Tom LaChance <guitarlanguageseries@gmail.com>
2018-01-15 15:32:28 -0600

    app.js now pushes the recent user, but we need to add a timestamp to figure out who was the most recent user

--- app.js
+++ app.js
@@ -8,3 +8,45 @@ var config = {
   messagingSenderId: "812504048941"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var name = "";
+var email = "";
+var age = 0;
+var comment = "";
+
+// cache jquery objects
+var $nameInput = $("#name-input");
+var $emailInput = $("#email-input");
+var $ageInput = $("#age-input");
+var $commentInput = $("#comment-input");
+var $addUserInput = $("#add-user");
+
+var $nameDisplay = $("#name-display");
+var $emailDisplay = $("#email-display");
+var $ageDisplay = $("#age-display");
+var $commentDisplay= $("#comment-display");
+
+
+$addUserInput.on("click", function(){
+    name = $nameInput.val().trim();
+    email = $emailInput.val().trim();
+    age = $ageInput.val().trim();
+    comment = $commentInput.val().trim();
+
+    firebase.database().ref().push({
+        name:name,
+        email:email,
+        age:age,
+        comment:comment
+    })
+});
+
+
+firebase.database().ref().on("value", function(snapshot){
+    $nameDisplay.html(snapshot.val().name);
+    $emailDisplay.html(snapshot.val().email);
+    $ageDisplay.html(snapshot.val().age);
+    $commentDisplay.html(snapshot.val().comment);
+});
