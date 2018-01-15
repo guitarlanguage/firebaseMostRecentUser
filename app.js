00108f7d9b51f07a74cb7ec7ab2578275e18f342
Tom LaChance <guitarlanguageseries@gmail.com>
2018-01-15 14:55:05 -0600

    added index and js file

---
+++ app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAHQRV5j02JxFak2xv_pBWWowsYb4WmdFw",
+  authDomain: "mostrecentuser-f4e38.firebaseapp.com",
+  databaseURL: "https://mostrecentuser-f4e38.firebaseio.com",
+  projectId: "mostrecentuser-f4e38",
+  storageBucket: "",
+  messagingSenderId: "812504048941"
+};
+firebase.initializeApp(config);
