05ffc6f8a1de8eccf232f5815aea667cd1768735
Tom LaChance <guitarlanguageseries@gmail.com>
2018-01-16 12:05:06 -0600

    added comments for later understanding

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var config = {
   messagingSenderId: "812504048941"
 };
 firebase.initializeApp(config);
-
+//variable to reference the firebase database
 var database = firebase.database();
-
+//global variables to send to firebase database
 var name = "";
 var email = "";
 var age = 0;
@@ -28,22 +28,26 @@ var $emailDisplay = $("#email-display");
 var $ageDisplay = $("#age-display");
 var $commentDisplay= $("#comment-display");
 
-
+//when addUserInput is clicked we collect data from form and store in variables
 $addUserInput.on("click", function(){
+    //Don't refresh the page
+    event.preventDefault();
     name = $nameInput.val().trim();
     email = $emailInput.val().trim();
     age = $ageInput.val().trim();
     comment = $commentInput.val().trim();
-
+    //push the data to firebase
     firebase.database().ref().push({
+        //give our properties a name
         name:name,
         email:email,
         age:age,
         comment:comment,
+        //TIMESTAMP functionality via firebase
         dateAdded:firebase.database.ServerValue.TIMESTAMP
     });
 });
-
+//post data from firebase into the .well class everytime a new child is added
 firebase.database().ref().on("child_added", function(snapshot){
     $(".well").append("<p>Name: " + snapshot.val().name + "</p>");
     $(".well").append("<p>Email: " + snapshot.val().email + "</p>");
@@ -51,9 +55,8 @@ firebase.database().ref().on("child_added", function(snapshot){
     $(".well").append("<p>Comment: " + snapshot.val().comment + "</p>");
     $(".well").append("<hr>");
 
-})
-
-
+});
+//orders by firebase property, dateAdded, and only grabs the last one
 firebase.database().ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot){
     $nameDisplay.html("<h3>Name: "+ snapshot.val().name + "</h3>");
     $emailDisplay.html("<h3>Email: "+ snapshot.val().email + "</h3>");
